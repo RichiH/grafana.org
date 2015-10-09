b0bb97c1c351e8a23a633855424075a10eef5d52
Trent White <trent@raintank.io>
2015-10-09 11:00:15 -0400

    Site updated to d080134

--- assets/bower_components/modernizr/feature-detects/css/transitions.js
+++
@@ -1,9 +0,0 @@
-/*!
-{
-  "name": "CSS Transitions",
-  "property": "csstransitions",
-  "caniuse": "css-transitions",
-  "tags": ["css"]
-}
-!*/
-define(["Modernizr","testAllProps"],function(n,t){n.addTest("csstransitions",t("transition","all",!0))});
