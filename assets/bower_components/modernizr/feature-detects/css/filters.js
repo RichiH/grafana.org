47b8e7217c6222e1b477337d8921c4ed7d5ab36b
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-10-27 11:03:27 +0100

    Site updated to 1011a66

--- assets/bower_components/modernizr/feature-detects/css/filters.js
+++ assets/bower_components/modernizr/feature-detects/css/filters.js
@@ -12,4 +12,4 @@
   }]
 }
 !*/
-define(["Modernizr","createElement","prefixes","testAllProps","test/css/supports"],function(e,t,n,r){e.addTest("cssfilters",function(){if(e.supports)return n("filter","blur(2px)");var i=t("a");return i.style.cssText=r.join("filter:blur(2px); "),!!i.style.length&&(void 0===document.documentMode||document.documentMode>9)})});
+define(["Modernizr","createElement","prefixes","testAllProps","test/css/supports"],function(e,t,n,i){e.addTest("cssfilters",function(){if(e.supports)return n("filter","blur(2px)");var r=t("a");return r.style.cssText=i.join("filter:blur(2px); "),!!r.style.length&&(void 0===document.documentMode||document.documentMode>9)})});
